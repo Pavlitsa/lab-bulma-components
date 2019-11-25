ccc0ecbb2473af13c3754c71567650a1036335a0
Pavlitsa <pzafeiri@gmail.com>
2019-11-25 19:16:17 +0100

    still in progress

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -138,10 +138,45 @@ const FormField = props => {
   );
 };
 
+const btnClasses = {
+  isActive: "is-active",
+  isBlack: "is-black",
+  isCentered: "is-centered",
+  isDanger: "is-danger",
+  isDark: "is-dark",
+  isFocused: "is-focused",
+  isGrouped: "is-grouped",
+  isHovered: "is-hovered",
+  isInfo: "is-info",
+  isInverted: "is-inverted",
+  isLarge: "is-large",
+  isLight: "is-light",
+  isLink: "is-link",
+  isLoading: "is-loading",
+  isMedium: "is-medium",
+  isOutlined: "is-outlined",
+  isPrimary: "is-primary",
+  isRight: "is-right",
+  isRounded: "is-rounded",
+  isSelected: "is-selected",
+  isSmall: "is-small",
+  isStatic: "is-static",
+  isSuccess: "is-success",
+  isText: "is-text",
+  isWarning: "is-warning",
+  isWhite: "is-white"
+};
+
 const CoolButton = props => {
-  return (
-    <button className="button is-rounded my-class is-danger is-small">{props.label}    </button>
-  );
+  console.log(props);
+  let classes = "button";
+  if (props.className) classes += ` ${props.className}`;
+  if (props.isInfo) classes += ` ${btnClasses.isInfo}`;
+  if (props.isSmall) classes += ` ${btnClasses.isSmall}`;
+  if (props.isDanger) classes += ` ${btnClasses.isDanger}`;
+  if (props.isSuccess) classes += ` ${btnClasses.isSuccess}`;
+
+  return <button className={classes}>{props.children}</button>; // children is the inner text/html
 };
 
 // We render the Navbar component inside the App component which we render in the index.js in order to show in the browser
@@ -149,18 +184,18 @@ const App = () => {
   return (
     <div>
       <Navbar />
-      <CoolButton isSmall isDanger className="is-rounded my-class" >
-        Button 1
-      </CoolButton>
-      <CoolButton isSmall isSuccess>
-        Button 2
-      </CoolButton>
       <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
       <FormField
         label="Email"
         type="email"
         placeholder="e.g. alexsmith@gmail.com"
       />
+      <CoolButton isSmall isDanger className="is-rounded my-class">
+        Button 1
+      </CoolButton>
+      <CoolButton isSmall isSuccess>
+        Button 2
+      </CoolButton>
     </div>
   );
 };
